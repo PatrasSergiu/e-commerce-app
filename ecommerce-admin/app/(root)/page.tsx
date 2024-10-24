316b04b9ca229fc3cf03252c00a0ec549b0f6965
PatrasSergiu <patras.sergiu@yahoo.com>
2024-10-24 23:22:45 +0300

    project setup
    created next app for admin project and installed :
    - tailwind
    - shadcn-ui

---
+++ ecommerce-admin/app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <p>Hello admin dashboard</p>
+      <Button className="mt-20">Click me</Button>
+    </>
+  );
+}
